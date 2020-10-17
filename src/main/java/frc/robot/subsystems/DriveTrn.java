72c3bb96a07a642f78eb4b603092aefe870c630a
ectsy <johnroy.tja@gmail.com>
2020-10-17 01:36:45 -0400

    1st commit

---
+++ src/main/java/frc/robot/subsystems/DriveTrn.java
@@ -0,0 +1,80 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotContainer;
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase; 
+public class DriveTrn extends SubsystemBase implements Runnable{
+  RobotContainer rc = new RobotContainer();
+  /**
+   * Creates a new DriveTrn.
+   */
+  public static WPI_TalonFX m_talon1;
+  public static WPI_TalonFX m_talon2;
+  public static WPI_TalonFX m_talon3;
+  public static WPI_TalonFX m_talon4;
+  public static WPI_TalonFX m_talon5;
+  public static WPI_TalonFX m_talon6;
+  NeutralMode brake = NeutralMode.Brake;
+  TalonFXInvertType kInvertType = TalonFXInvertType.CounterClockwise;
+  
+  //SpeedControllerGroup left = new SpeedControllerGroup(m_talon1, m_talon2, m_talon5);
+  //SpeedControllerGroup right = new SpeedControllerGroup(m_talon3, m_talon4, m_talon6);
+
+  public DriveTrn() {
+
+m_talon1 = new WPI_TalonFX(1);
+  m_talon2 = new WPI_TalonFX(2); 
+  m_talon3 = new WPI_TalonFX(3);
+  m_talon4 = new WPI_TalonFX(4);
+  m_talon5 = new WPI_TalonFX(5);
+  m_talon6 = new WPI_TalonFX(6);
+
+m_talon1.setInverted(false);
+m_talon2.setInverted(false);
+m_talon5.setInverted(false);
+m_talon3.setInverted(true);
+m_talon4.setInverted(true);
+m_talon6.setInverted(true);  
+
+m_talon1.setNeutralMode(brake);
+m_talon2.setNeutralMode(brake);
+m_talon3.setNeutralMode(brake);
+m_talon4.setNeutralMode(brake);
+m_talon5.setNeutralMode(brake);  
+m_talon6.setNeutralMode(brake);
+
+
+m_talon1.setInverted(kInvertType);
+m_talon2.setInverted(kInvertType);
+m_talon5.setInverted(kInvertType);
+  Thread DT = new Thread(this);
+  DT.start();
+  }
+  public void run(){
+    while(true){
+rc.drive(rc.driveController().getX(Hand.kRight), rc.driveController().getY(Hand.kLeft));
+    }
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    
+    
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -0,0 +1,85 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotContainer;
+
+
+
+public class Limelight extends SubsystemBase implements Runnable{
+  RobotContainer rc = new RobotContainer();
+
+
+  final double h2 = 90.875; //height of target
+  final double h1 = 14.75; //height of limeligt
+  
+  double hoodAngle;
+  double calculateAngle;
+
+  double limeTarget;
+  /**
+   * Creates a new Limelight.
+   */
+  public Limelight() {
+    Thread LL = new Thread(this);
+  }
+  public void run(){
+    while(true){
+      final double a2 = Math.toRadians(NetworkTableInstance.getDefault().getTable("limelight").getEntry("ty").getDouble(0));
+    final double a1 = Math.toRadians(13); //angle of limelight
+  
+    final double heightValue = (h2 - h1);
+    
+    final double angleboth = a1 + a2;
+    final double tanValue = Math.tan(angleboth);
+  
+    double distanceFromTarget = heightValue/tanValue;
+  
+  
+
+
+    if (rc.driveController().getRawAxis(3) > 0.65){
+      NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(0);
+      NetworkTableInstance.getDefault().getTable("limelight").getEntry("camMode").setNumber(0);
+    } else if (rc.driveController().getRawAxis(3) < 0.6) {
+      NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(1);
+      NetworkTableInstance.getDefault().getTable("limelight").getEntry("camMode").setNumber(1);
+    }
+    limelightTracking();
+    double tx = (NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0));
+  double hordis = Math.abs(tx);
+  double steer = .055; 
+limeTarget = tx * steer; 
+if(rc.driveController().getRawAxis(3) > 0.7 && (NetworkTableInstance.getDefault().getTable("limelight").getEntry("tv").getDouble(0) == 1)) {
+  if (hordis > 1 || hordis < -1) {
+  if(tx < 5 && tx > -5){
+        rc.drive((tx * .1 ), 0);
+      }else{
+        rc.drive(limeTarget, 0);
+      }    
+    }
+  }else{
+   
+      rc.drive((rc.driveController().getX(Hand.kRight)), -(rc.driveController().getY(Hand.kLeft)));
+
+  }
+    }
+  }
+  
+  @Override
+  public void periodic() {
+  }
+
+  public void limelightTracking(){
+    double tx = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0);
+    double tv = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tv").getDouble(0);
+  }
+}
