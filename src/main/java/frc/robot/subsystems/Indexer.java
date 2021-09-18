85e58676f8459ab6fcab38873f8b59b83d360358
John Roy <61093089+John-Roy123@users.noreply.github.com>
2021-09-18 14:48:24 -0400

    Update Indexer.java

--- src/main/java/frc/robot/subsystems/Indexer.java
+++ src/main/java/frc/robot/subsystems/Indexer.java
@@ -15,7 +15,7 @@ import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotContainer;
 
-public class Indexer extends SubsystemBase implements Runnable{
+public class Indexer extends SubsystemBase{
 
   RobotContainer rc = new RobotContainer();
   Collector C = new Collector();
@@ -43,20 +43,6 @@ public class Indexer extends SubsystemBase implements Runnable{
    * Creates a new Feeder.
    */
   public Indexer() {
-  
-    Thread x = new Thread(this);
-    x.run();
-  }
-
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-
-  }
-
-  @Override
-  public void run() {
-    // TODO Auto-generated method stub
     m_indexer = new WPI_TalonSRX(indexerCANID);
 
     s_ultra1 = new Ultrasonic(7, 6);
@@ -66,42 +52,47 @@ public class Indexer extends SubsystemBase implements Runnable{
     s_ultra2.setAutomaticMode(true);
 
     t_ultra1 = 0;
-while(true){
-      if(s_ultra1Range){
-        if(t_indexreset == 1){
-          ballcount++;
-          t_indexreset++;
-        }else{
-          t_indexreset++;
-        }
-      }
-
-      if(rc.operateController().getRawAxis(3) > 5){
-        m_feeder.set(-1);
-        m_indexer.set(.7); 
-        ballcount = 0;
-      }
-      else if (rc.operateController().getBumper(Hand.kLeft)){
-        m_indexer.set(-0.3);
-        C.m_collector.set(-1);
-        m_feeder.set(.7);
-      }else if(rc.operateController().getBumper(Hand.kRight)){
-        m_indexer.set(0.3);
-        C.m_collector.set(1);
-     }else if(s_ultra1.getRangeInches() < 5 && !(s_ultra2.getRangeInches() < 5 )){
-      m_indexer.set(.4);
-      collecting = true;
-    }else { 
-      if(t_ultra1 < 6){
-        m_feeder.set(-.2);
-      } else if(collecting && s_ultra2.getRangeInches() < 8){
-        t_ultra1 = 0;
-      } else {
-        collecting = false;
-        m_feeder.set(0);                                     
-      }
-      m_indexer.set(0);
-    }
+  
   }
-}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    if(s_ultra1Range){
+      if(t_indexreset == 1){
+        ballcount++;
+        t_indexreset++;
+      }else{
+        t_indexreset++;
+      }
+    }
+
+    if(rc.operateController().getRawAxis(3) > 5){
+      m_feeder.set(-1);
+      m_indexer.set(.7); 
+      ballcount = 0;
+    }
+    else if (rc.operateController().getBumper(Hand.kLeft)){
+      m_indexer.set(-0.3);
+      C.m_collector.set(-1);
+      m_feeder.set(.7);
+    }else if(rc.operateController().getBumper(Hand.kRight)){
+      m_indexer.set(0.3);
+      C.m_collector.set(1);
+   }else if(s_ultra1.getRangeInches() < 5 && !(s_ultra2.getRangeInches() < 5 )){
+    m_indexer.set(.4);
+    collecting = true;
+  }else { 
+    if(t_ultra1 < 6){
+      m_feeder.set(-.2);
+    } else if(collecting && s_ultra2.getRangeInches() < 8){
+      t_ultra1 = 0;
+    } else {
+      collecting = false;
+      m_feeder.set(0);                                     
+    }
+    m_indexer.set(0);
+  }
+  }
+
 }
